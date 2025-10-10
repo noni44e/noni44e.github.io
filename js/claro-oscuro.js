a9309cbb8cc91da5a9e2610c1581f500cbd78726
LuisDiaz <luisdiaztrabajos@gmail.com>
2025-10-10 18:21:19 -0300

    Update claro-oscuro.js

--- js/claro-oscuro.js
+++ js/claro-oscuro.js
@@ -5,15 +5,18 @@ function temaOscuro() {
 
   if (temaActual === "light") {
     body.setAttribute("data-bs-theme", "dark");
-    icono.classList.remove("bi-sun");
-    icono.classList.add("bi-moon-stars-fill");
+    if (icono) {
+      icono.classList.remove("bi-sun");
+      icono.classList.add("bi-moon-stars-fill");
+    }
   } else {
     body.setAttribute("data-bs-theme", "light");
-    icono.classList.remove("bi-moon-stars-fill");
-    icono.classList.add("bi-sun");
+    if (icono) {
+      icono.classList.remove("bi-moon-stars-fill");
+      icono.classList.add("bi-sun");
+    }
   }
 
-  // Guardar preferencia
   localStorage.setItem("tema", body.getAttribute("data-bs-theme"));
 }
 
@@ -22,5 +25,7 @@ document.addEventListener("DOMContentLoaded", () => {
   document.body.setAttribute("data-bs-theme", temaGuardado);
 
   const icono = document.querySelector(".dl-icon");
-  icono.classList.add(temaGuardado === "dark" ? "bi-moon-stars-fill" : "bi-sun");
+  if (icono) {
+    icono.classList.add(temaGuardado === "dark" ? "bi-moon-stars-fill" : "bi-sun");
+  }
 });
